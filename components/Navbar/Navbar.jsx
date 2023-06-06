f2975e3df1a8ab9bc31417ec63d4990643d4cac9
iagoSiccardi <iago.siccardi@gmail.com>
2023-06-06 20:35:46 -0300

    Responsive done

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,21 +1,39 @@
+'use client'
+
+import { FaBars } from 'react-icons/fa'
 import Link from 'next/link'
 import styles from './Navbar.module.css'
+import { useState } from 'react'
 
 const Navbar = () => {
-  return (
-    <header className={styles.header}>
-        <section className={styles.section}>
-        <Link className={styles.ecommerce} href='/'>Hotel</Link>
-        <ul>
-            <Link href='/habitaciones'>Habitaciones</Link>
-            <Link href='/ubicacion'>Ubicacion</Link>
-            <Link href='/about'>Sobre nosotros</Link>
-            <Link href='/contact'>Contacto</Link>
+	const [active, setActive] = useState(false)
 
-        </ul>
-        </section>
-    </header>
-  )
+	return (
+		<header className={styles.header}>
+			<Link className={styles.ecommerce} href='/'>
+				Hotel
+			</Link>
+			<section className={styles.section}>
+				<ul className={styles.fwUl}>
+					<Link href='/habitaciones'>Habitaciones</Link>
+					<Link href='/ubicacion'>Ubicacion</Link>
+					<Link href='/about'>Sobre nosotros</Link>
+					<Link href='/contact'>Contacto</Link>
+				</ul>
+			</section>
+			{active ? (
+				<section className={styles.smSection}>
+					<ul className={styles.smUl}>
+						<Link href='/habitaciones'>Habitaciones</Link>
+						<Link href='/ubicacion'>Ubicacion</Link>
+						<Link href='/about'>Sobre nosotros</Link>
+						<Link href='/contact'>Contacto</Link>
+					</ul>
+				</section>
+			) : null}
+			<FaBars size='2rem' onClick={() => setActive(!active)} />
+		</header>
+	)
 }
 
 export default Navbar
